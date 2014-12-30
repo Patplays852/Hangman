a040894e0af8fc3ef1686e91e4a650aa1323861e
valnakrume <valnakrume@hotmail.com>
2014-12-30 11:38:02 -0500

    Hide the word list

--- Hangman/Form1.Designer.vb
+++ Hangman/Form1.Designer.vb
@@ -195,6 +195,9 @@ Partial Class Form1
         Me.ListBox1.Name = "ListBox1"
         Me.ListBox1.Size = New System.Drawing.Size(120, 95)
         Me.ListBox1.TabIndex = 14
+        Me.ListBox1.TabStop = False
+        Me.ListBox1.UseTabStops = False
+        Me.ListBox1.Visible = False
         '
         'TextBox1
         '
